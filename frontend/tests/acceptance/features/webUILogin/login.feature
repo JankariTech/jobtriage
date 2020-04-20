2c30f6979179af5989ad7ceb6c39abfea1f2265d
HariBhandari07 <bhandarihari07@gmail.com>
2020-04-20 11:45:49 +0545

    Fix and optimize the acceptance test and infrastructures

--- frontend/tests/acceptance/features/webUILogin/login.feature
+++ frontend/tests/acceptance/features/webUILogin/login.feature
@@ -5,7 +5,7 @@ Feature: login a user
   Scenario: signup and login a user
     Given the user has browsed to the homepage
     When the user browses to the signup page using the webUI
-    And the user signs up with name "user2", email "user2@gmail.com" and password "hari" using the webUI
+    And the user signs up with name "user1", email "user1@gmail.com" password "password" and confirmation password "password" using the webUI
     And the user browses to the login page
     And the user logs in with email "user2@gmail.com" and password "hari" using the webUI
     Then user should be redirected to the dashboard page
---
+++ frontend/tests/acceptance/step_definitions/loginContext.js
@@ -0,0 +1,21 @@
+const { I } = inject();
+const loginPage = require('../pages/loginPage');
+const dashboard = require('../pages/dashboardPage');
+const signupPage = require('../pages/signupPage');
+
+Given('the user has browsed to the homepage', () => I.amOnPage('/'));
+
+When('the user browses to the signup page using the webUI', () => I.amOnPage(signupPage.url));
+
+When('the user signs up with name {string}, email {string} password {string} and confirmation password {string} using the webUI',
+  (name, email, password, confirmationPassword) => {
+    signupPage.signUp(name, email, password, confirmationPassword);
+  });
+
+When('the user browses to the login page', () => I.amOnPage(loginPage.url));
+
+When('the user logs in with email {string} and password {string} using the webUI', (email, password) => {
+  loginPage.login(email, password);
+});
+
+Then('user should be redirected to the dashboard page', () => I.seeElement(dashboard.dashboardContainer));
